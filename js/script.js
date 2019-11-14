f20e58a53b6df48f42da63a7fe7014a116a3525c
Redvan <redvanisation@gmail.com>
2019-11-14 18:47:03 -0500

    Fix linter errors

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(name, author, pages, status) {
-
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 }
+
 const btnNewBook = document.querySelector('#newbook');
 const btn = document.querySelector('#add-book');
 const cancel = document.querySelector('#cancel-book');
@@ -16,79 +15,75 @@ const bookForm = document.querySelector('#book-form');
 const background = document.querySelector('.form-background');
 
 
+function showNewBook() {
+  bookForm.classList.remove('hidden');
+  bookForm.classList.add('show');
+
+  background.classList.remove('hiddenbg');
+  background.classList.add('showbg');
+} 
+
+function hideNewBook() {
+  bookForm.classList.remove('show');
+  bookForm.classList.add('hidden');
+
+  background.classList.remove('showbg');
+  background.classList.add('hiddenbg');
+}
+
+function render(arr, elem) {
+  elem.innerHTML = '';
+
+  arr.forEach((book) => {
+      elem.innerHTML += `<tr> 
+                            <td> 
+                                <p class=" form-content">${book.name}</p>  
+                            </td>
+                            <td> 
+                                <p class="form-content">${book.author}</p> 
+                            </td>
+                            <td> 
+                                <p class="form-content">${book.pages}</p>  
+                            </td>
+                            <td>                   
+                                <button class="form-content ui button" id="change-status" onclick="changeStatus(${arr.indexOf(book)}, myLibrary)">${book.status}</button> 
+                            </td>
+                            <td>
+                                <button class="form-content ui button" id="remove-book" onclick="removeBook(${arr.indexOf(book)}, myLibrary)">X</button>
+                            </td>
+                          </tr>`;
+                        });
+}
+
+function changeStatus(index, arr) {
+  arr[index].status === 'Read' ? arr[index].status = 'Not Read' : arr[index].status = 'Read';
+  render(myLibrary, booksDiv);
+}
+
+function removeBook(index, arr) {
+  arr.splice(index, 1);
+  render(myLibrary, booksDiv);
+}
+
+function addBook() {
+  const name = document.querySelector('#book-name').value;
+  const author = document.querySelector('#book-author').value;
+  const pages = document.querySelector('#book-pages').value;
+  const status = document.querySelector('input[name="book-status"]');
+
+  status.checked ? status.value = "Read" : status.value = "Not Read";
+
+  if (!name && !author && !pages) return alert("Please enter all book details");
+
+
+  myLibrary.push( new Book(name, author, pages, status.value));
+  render(myLibrary, booksDiv);
+
+  hideNewBook();
+}
 
 btn.addEventListener('click', addBook);
 cancel.addEventListener('click', hideNewBook);
 
 btnNewBook.addEventListener('click', showNewBook);
 background.addEventListener('click', hideNewBook);
-
-function showNewBook(){
-    bookForm.classList.remove('hidden');
-    bookForm.classList.add('show');
-
-    background.classList.remove('hiddenbg');
-    background.classList.add('showbg');
-} 
-
-function hideNewBook() {
-    bookForm.classList.remove('show');
-    bookForm.classList.add('hidden');
-
-    background.classList.remove('showbg');
-    background.classList.add('hiddenbg');
-}
-
-function render(arr, elem) {
-    elem.innerHTML = '';
-
-    arr.forEach(book => {
-        elem.innerHTML += `   
-            <tr> 
-                <td> 
-                    <p class=" form-content">${book.name}</p>  
-                </td>
-                <td> 
-                    <p class="form-content">${book.author}</p> 
-                </td>
-                <td> 
-                    <p class="form-content">${book.pages}</p>  
-                </td>
-                <td>                   
-                    <button class="form-content ui button" id="change-status" onclick="changeStatus(${arr.indexOf(book)}, myLibrary)">${book.status}</button> 
-                </td>
-                <td>
-                    <button class="form-content ui button" id="remove-book" onclick="removeBook(${arr.indexOf(book)}, myLibrary)">X</button>
-                </td>
-            </tr>`;
-    });
-}
-
-function changeStatus(index, arr) { 
-    arr[index].status === 'Read' ? arr[index].status = 'Not Read' : arr[index].status = 'Read';
-    render(myLibrary, booksDiv);
-}
-
-function removeBook(index, arr){
-    arr.splice(index, 1);
-    render(myLibrary, booksDiv);
-    console.log(arr.length);
-}
-
-function addBook() {
-    const name = document.querySelector('#book-name').value;
-    const author = document.querySelector('#book-author').value;
-    const pages = document.querySelector('#book-pages').value;
-    const status = document.querySelector('input[name="book-status"]');
-    
-    status.checked ? status.value = "Read" : status.value = "Not Read";
-  
-    if (!name && !author && !pages) return alert("Please enter all book details");
-    
-
-    myLibrary.push( new Book(name, author, pages, status.value));
-    render(myLibrary, booksDiv);
-
-    hideNewBook();
-
-}
