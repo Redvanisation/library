cd5967ef38bfadf0491ce56e80b27d601e682ab6
AnthonyTC89 <ptonyptc@gmail.com>
2019-11-14 13:56:55 -0500

    remove funtion created

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
 let myLibrary = [];
 
+function Book(name, author, pages, status) {
+
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+
+}
+const btnNewBook = document.querySelector('#newbook');
 const btn = document.querySelector('#add-book');
 const div = document.querySelector('#books-div');
+//const removeBook = document.querySelector('#remove-book');
+
 
 btn.addEventListener('click', addBook);
+btnNewBook.addEventListener('click', showNewBook);
+//removeBook.addEventListener('click', removeBook);
+
+function removeBook(book){
+    console.log(book);
+}
+
+function showNewBook(){
+    //document.querySelector('#book-form').visible = true;
+} 
 
 function render(arr, elem) {
     elem.innerHTML = '';
     arr.forEach(book => {
-        elem.innerHTML += `<h3 class="title">${book}</h3>`;
+        elem.innerHTML += `<h3 class="title">Name: ${book.name}</h3> 
+                            <h4 class="title">Author: ${book.author}</h4>
+                            <h4 class="title">Pages: ${book.pages}</h4>
+                            <h4 class="title">Status: ${book.status}</h4>
+                            <button id="remove-book" onclick="removeBook(elem)">Remove</button>`;
     });
 }
 
-function addBook() {
-    const book = document.querySelector('#book-input').value;
+function removeBook(){
+    console.log("remove");
+}
 
-    if (!book) return  alert("Please enter a book title");
+function addBook() {
+    const name = document.querySelector('#book-name').value;
+    const author = document.querySelector('#book-author').value;
+    const pages = document.querySelector('#book-pages').value;
+    const status = document.querySelector('input[name="book-status"]:checked').value;
+
+    if (!name && !author && !pages && !status) return alert("Please enter all book details");
     
-    myLibrary.push(book);
+
+    myLibrary.push( new Book(name, author, pages, status));
     console.log(myLibrary);
     console.log(render(myLibrary, div));
+    console.log(status);
 }
